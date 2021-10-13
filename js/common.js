960b625059a74a7c6f760abb15ebc9a56e6eb090
Eulga <63146118+Eulga@users.noreply.github.com>
2021-10-13 16:38:14 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,10 @@
-// $(window).on('load', function() {
-//     setTimeout (function () {
-//         scrollTo(0,0);
-//         }, 100);
-// })
-
+$('#gnb > ul > li').on('mouseenter', function() {
+    $(this).children('div').stop().animate({height: 350}).children('.depth2').stop().slideDown()
+    // $(this).children('div').stop().toggle('blind')
+    $('#header').css({borderBottom: '1px solid #000', borderRadius: '15px 15px 0 0'})
+})
+$('#gnb > ul > li').on('mouseleave', function() {
+    $(this).children('div').stop().animate({height: 0}, 300).children('.depth2').stop().css({display: 'none'});
+    // $(this).children('div').stop().toggle('blind')
+    $('#header').css({borderBottom: 0, borderRadius: '15px'})
+})
